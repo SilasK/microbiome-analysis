84fae41ba7c1e2376e9a20c6201cd049515404ff
kiesers <silas.kieser@gmail.com>
2020-06-16 15:42:44 +0200

    update to new aldex version

--- scripts/Rscripts/aldex.R
+++ scripts/Rscripts/aldex.R
@@ -15,41 +15,50 @@ Comparisons= snakemake@config[['Comparisons']]
 for (comparison in names(Comparisons))
 {
   print(comparison)
-  
+
   selected= grouping_variable %in% Comparisons[[comparison]]
-  
+
   conditions = grouping_variable[selected]
-  
+
   x <- aldex.clr(reads= t(data[selected,]), conds= conditions, mc.samples=128,denom='all',useMC=TRUE)
-  
-  d.eff <- aldex.effect(x, conditions,useMC = T,include.sample.summary = T)
-  
-  if (! all(sort(Comparisons[[comparison]]) == Comparisons[[comparison]]))
-  {
-    # alphanumeric sorting will inverse comparison
-    
-    d.eff$effect= - d.eff$effect
-    d.eff$diff.btw= - d.eff$diff.btw
-    
-  }
-  
-  
-  
-  d.tt <- aldex.ttest(x, conditions)
+
+  d.eff <- aldex.effect(x,useMC = T)
+
+
+
+  d.tt <- aldex.ttest(x)
   res.all <- data.frame(d.eff,d.tt)
   # get 'significant' set
-  
+
   res.all$label = rep(0,nrow(res.all))
   res.all[res.all$wi.eBH < 0.1,"label"] = "Significant"
   res.all[which(res.all$label == 0), "label"] = "Not significant"
-  
-  
+
+
   write.table(res.all,paste0(snakemake@params$output_folder,'/',comparison,'/stats_aldex.tsv')   ,sep='\t')
-  
+
   write.table(res.all[res.all$wi.eBH < 0.1,],paste0(snakemake@params$output_folder,'/',comparison,'/sig.tsv')   ,sep='\t')
-  
-   
+
+
 }
 
 
-# 
+
+  d.tt <- aldex.ttest(x, conditions)
+  res.all <- data.frame(d.eff,d.tt)
+  # get 'significant' set
+
+  res.all$label = rep(0,nrow(res.all))
+  res.all[res.all$wi.eBH < 0.1,"label"] = "Significant"
+  res.all[which(res.all$label == 0), "label"] = "Not significant"
+
+
+  write.table(res.all,paste0(snakemake@params$output_folder,'/',comparison,'/stats_aldex.tsv')   ,sep='\t')
+
+  write.table(res.all[res.all$wi.eBH < 0.1,],paste0(snakemake@params$output_folder,'/',comparison,'/sig.tsv')   ,sep='\t')
+
+
+}
+
+
+#
